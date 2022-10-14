748a169edb133173c99cd8d24facb387f7bb8600
Oglo12 <ogloooglooagla@gmail.com>
2022-10-14 03:14:29 -0500

    Easy way to update configs! :-D

---
+++ update-configs.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+notify-send "Rice Update Script" "Updating Qtile."
+cd ~/.config/qtile/
+git pull origin main
+
+notify-send "Rice Update Script" "Updating Picom."
+cd ~/.config/picom/
+git pull origin main
+
+notify-send "Rice Update Script" "Updating Dunst."
+cd ~/.config/dunst/
+git pull origin main
+
+notify-send "Rice Update Script" "Updating Rofi."
+cd ~/.config/rofi/
+git pull origin main
+
+cd ~
+
+notify-send "Rice Update Script" "Done! :-D"
