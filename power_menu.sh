ad163384c9502f30a7f294e42e711a7b8ce9b511
Oglo12 <ogloooglooagla@gmail.com>
2022-11-02 14:10:52 -0500

    Better programming practices!

--- power_menu.sh
+++ power_menu.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-chosen=$(echo -e "   Power Off\n   Restart\n   Lock\n   Log Out" | rofi -dmenu -i -p "Power Menu:")
+poweroff_text="   Power Off"
+reboot_text="   Restart"
+lock_text="   Lock"
+logout_text="   Log Out"
 
-if [[ $chosen = "   Power Off" ]]; then
+chosen=$(echo -e "$poweroff_text\n$reboot_text\n$lock_text\n$logout_text" | rofi -dmenu -i -p "Power Menu:")
+
+if [[ $chosen = $poweroff_text ]]; then
 	poweroff
-elif [[ $chosen = "   Restart" ]]; then
+elif [[ $chosen = $reboot_text ]]; then
 	reboot
-elif [[ $chosen = "   Lock" ]]; then
+elif [[ $chosen = $lock_text ]]; then
 	betterlockscreen -l
-elif [[ $chosen = "   Log Out" ]]; then
+elif [[ $chosen = $logout_text ]]; then
 	pkill -KILL -u $USER
 fi
