e37205019c10872cf857a5926e7abe0f3beb329f
Oglo12 <ogloooglooagla@gmail.com>
2022-11-02 14:05:02 -0500

    Fancy power menu!

--- power_menu.sh
+++ power_menu.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-# Simple script to handle a DIY shutdown menu. When run you should see a bunch of options (shutdown, reboot etc.)
-#
-# Requirements:
-# - rofi
-# - systemd, but you can replace the commands for OpenRC or anything else
-#
-# Instructions:
-# - Save this file as power.sh or anything
-# - Give it exec priviledge, or chmod +x /path/to/power.sh
-# - Run it
+chosen=$(echo -e "   Power Off\n   Restart\n   Lock\n   Log Out" | rofi -dmenu -i -p "Power Menu:")
 
-chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nHibernate\nHybrid-sleep\nSuspend-then-hibernate" | rofi -dmenu -i -p "Power Menu:")
-# Info about some states are available here:
-# https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
-
-if [[ $chosen = "Logout" ]]; then
-	jwm -exit
-elif [[ $chosen = "Shutdown" ]]; then
-	systemctl poweroff
-elif [[ $chosen = "Reboot" ]]; then
-	systemctl reboot
-elif [[ $chosen = "Suspend" ]]; then
-	systemctl suspend
-elif [[ $chosen = "Hibernate" ]]; then
-	systemctl hibernate
-elif [[ $chosen = "Hybrid-sleep" ]]; then
-	systemctl hibernate
-elif [[ $chosen = "Suspend-then-hibernate" ]]; then
-	systemctl suspend-then-hibernate
+if [[ $chosen = "   Power Off" ]]; then
+	poweroff
+elif [[ $chosen = "   Restart" ]]; then
+	reboot
+elif [[ $chosen = "   Lock" ]]; then
+	betterlockscreen -l
+elif [[ $chosen = "   Log Out" ]]; then
+	pkill -KILL -u $USER
 fi
